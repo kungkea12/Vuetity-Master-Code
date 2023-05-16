35657fa0f0673f9f0018f2388355e265a5f32372
kungkea12 <thkungkea1998@gmail.com>
2023-05-16 14:01:22 +0700

    Master Code

---
+++ src/components/NavBar.vue
@@ -0,0 +1,32 @@
+<template>
+    <v-app-bar color="white" app dark flat>
+      <v-icon color="black">fas fa-long-arrow-alt-left</v-icon>
+      <h4 class="black--text ml-3">Master Code</h4>
+      <v-spacer></v-spacer>
+      <h4 class="green--text text--lighten-2">New Developer</h4>
+      <v-icon x-small color="#42b883" class="ml-2 mr-2">fas fa-caret-down</v-icon>
+      <v-btn color="#42b883" class="withoutupercase">
+        <v-icon left>fas fa-plus</v-icon> New Project
+      </v-btn>
+      <v-badge dot overlap color="green" class="ml-3 mr-2" dark>
+        <v-avatar tile color="#e5f1ff">
+          <v-icon color="#42b883">fas fa-bell</v-icon>
+        </v-avatar>
+      </v-badge>
+      <v-avatar tile color="#e5f1ff">
+        <v-img src="p.png"></v-img>
+      </v-avatar>
+    </v-app-bar>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+  .v-btn.withoutupercase {
+    text-transform: none !important;
+  }
+</style>
---
+++ src/pages/HomePage.vue
@@ -0,0 +1,43 @@
+<template>
+    <v-app :style="{background: $vuetify.theme.themes.dark.background}">
+      <SideBar /> 
+      <NavBar />
+      <v-container>
+        <v-row>
+          <v-col cols="12" sm="9">
+            <v-row>
+              <v-col cols="12" sm="12">
+                <ConTact />
+              </v-col>
+              <v-col cols="12" sm="12">
+                <AgenDa />
+              </v-col>
+            </v-row>
+          </v-col>
+          <v-col cols="12" sm="3">
+            <DocTor /> 
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-app>
+</template>
+
+<script>
+import AgenDa from '@/components/AgenDa.vue'
+import ConTact from '@/components/ConTact.vue'
+import NavBar from '../components/NavBar.vue'
+import SideBar from '../components/SideBar.vue'
+import DocTor from '@/components/DocTor.vue'
+export default {
+  components: { 
+    SideBar, 
+    NavBar, 
+    ConTact, 
+    AgenDa, 
+    DocTor 
+  },
+}
+</script>
+
+<style>
+</style>
